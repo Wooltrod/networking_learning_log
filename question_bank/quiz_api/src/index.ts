e0f4f5b4b26b55277c897b2c7f01c23334e8ac7d
Wooltrod <ianadoyo64@gmail.com>
2026-05-11 15:14:10 +0300

    Import and read json file using fs module

--- question_bank/quiz_api/src/index.ts
+++ question_bank/quiz_api/src/index.ts
@@ -2,23 +2,9 @@ import express from 'express';
 import * as fs from 'fs';
 import * as path from 'path'
 
-interface Questions {
-    id: string;
-    deck: string;
-    deck_tag: string;
-    question: string;
-    answer: string[];
-    straight_forward: boolean;
-    day_number: string;
-    multiple_ans: boolean;
-    alligned: boolean;
-    tags: string[];
-    number: number;
-}
-
 const app = express();
 const port = 3001;
-const questions = require('..ccna.json') as Questions[];
+const data = JSON.parse(fs.readFileSync('ccna.json', 'utf-8'));
 
 
 console.log('Hello, World!');
