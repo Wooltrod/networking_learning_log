268368ebfece3adffeae72153b6a99c3310a3d0a
Wooltrod <ianadoyo64@gmail.com>
2026-05-11 14:51:25 +0300

    Import json data file by manually specifying types

--- question_bank/quiz_api/src/index.ts
+++ question_bank/quiz_api/src/index.ts
@@ -1,1 +1,24 @@
 import express from 'express';
+import * as fs from 'fs';
+import * as path from 'path'
+
+interface Questions {
+    id: string;
+    deck: string;
+    deck_tag: string;
+    question: string;
+    answer: string[];
+    straight_forward: boolean;
+    day_number: string;
+    multiple_ans: boolean;
+    alligned: boolean;
+    tags: string[];
+    number: number;
+}
+
+const app = express();
+const port = 3001;
+const questions = require('..ccna.json') as Questions[];
+
+
+console.log('Hello, World!');
