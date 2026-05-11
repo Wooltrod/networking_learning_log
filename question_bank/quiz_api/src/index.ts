5ea3b83bccace3f3a264d87273d2d341d092cde8
Wooltrod <ianadoyo64@gmail.com>
2026-05-11 15:53:58 +0300

    Edit index.ts

--- question_bank/quiz_api/src/index.ts
+++ question_bank/quiz_api/src/index.ts
@@ -2,7 +2,19 @@ import express from 'express';
 import * as fs from 'fs';
 import * as path from 'path'
 
-
+interface Questions {
+    id: string;
+    deck: string;
+    deck_tag: string;
+    question: string;
+    answer: string[];
+    straight_forward: boolean;
+    day_number: string;
+    multiple_ans: boolean;
+    alligned: boolean;
+    tags: string[];
+    number: number;
+}
 
 const app = express();
 const port = 3001;
