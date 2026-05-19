35e6f72a623336218888ada8777dd493d762d74b
Wooltrod <ianadoyo64@gmail.com>
2026-05-19 16:23:43 +0300

    Define two GET route/endpoints with JSON filtering

--- question_bank/quiz_api/src/index.ts
+++ question_bank/quiz_api/src/index.ts
@@ -20,6 +20,32 @@ const app = express();
 const port = 3001;
 const data: Questions[] = JSON.parse(fs.readFileSync('ccna.json', 'utf-8'));
 
-const config_questions = data.filter((item) => item.number == 200);
+// const config_questions = data.filter((item) => item.number == 200);
 
-console.log(config_questions);
+// console.log(config_questions);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.get('/api/all', (req, res) => {
+    res.json(data);
+});
+
+app.get('/api/:number', (req, res) => {
+    const { number } = req.params;
+    const question = data.filter((item) => item.number === Number(number));
+    if (!question) {
+        return res.status(404).json({ error: 'Question not found' });
+    }
+    res.json(question);
+});
+
+app.get('/api/day_number/:day_number', (req, res) => {
+    const { day_number } = req.params;
+    const question = data.filter((item) => item.day_number === day_number);
+    if (!question) {
+        return res.status(404).json({ error: 'Question not found' });
+    }
+    res.json(question);
+});
